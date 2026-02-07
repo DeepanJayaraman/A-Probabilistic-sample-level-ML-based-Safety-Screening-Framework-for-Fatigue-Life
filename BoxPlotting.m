47fc0bc4908dcbb2fd1b7cd514a982acae718947
Deepan Jayaraman <64420389+DeepanJayaraman@users.noreply.github.com>
2026-02-07 23:51:16 +0530

    BoxPlotting.m

--- BoxPlotting.m
+++ BoxPlotting.m
@@ -2,11 +2,9 @@
 clear;
 close all;
 
-CaseNs = [ ...
-    "Nf_1.633 GPa", "Nf_1.552 GPa", "Nf_1.452 GPa", "Nf_1_371GPa"...
-    "Nf_0.980 GPa", "Nf_0.949 GPa", "Nf_0.928 GPa" ];
+CaseNs = [ Add the case Names in string ];
 
-FolderN = "C:\Users\jayard4\OneDrive - Caterpillar\Research\Probabilistic SN\UQ-main\results\2 Extreme\";%\
+FolderN = Folder Name; 
 Xtick =  {'5+2 Extremes','7+2 Extremes','9+2 Extremes','11+2 Extremes'}; 
 % Xtick =  {'5+1 Extreme','7+1 Extreme','9+1 Extreme','11+1 Extreme'}; 
 for i  = 4
